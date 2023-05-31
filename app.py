6de42b84dd1bd7c1ed34c701c68d7c8d92495a62
Kumar <varkumar@deloitte.com>
2023-05-31 16:40:01 +0800

    v3.3 with prompt engg.

--- app.py
+++ app.py
@@ -1,27 +1,57 @@
-@app.route("/message", methods=["POST"])
+from flask import Flask, request, jsonify, render_template
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/message', methods=['POST'])
 def chat_message():
-    user_message = request.json["message"]
+    user_message = request.json['message']
+    messages = [
+        {
+            "role": "system",
+            "content": "You are an AI specialized in cloud infrastructure.",
+        },
+        {"role": "user", "content": user_message},
+    ]
+
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages,
+    )
+
+    bot_response = response['choices'][0]['message']['content']
 
     if is_cloud_infrastructure_question(user_message):
-        messages = [
-            {
-                "role": "system",
-                "content": "You are an AI specialized in cloud infrastructure.",
-            },
-            {"role": "user", "content": user_message},
-        ]
-
-        model_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-
-        bot_response = model_response["choices"][0]["message"]["content"]
-
-        # write chat history to a file
-        with open("/app/history/chat-history.txt", "a") as file:
-            file.write("User: " + user_message + "\n")
-            file.write("AI: " + bot_response + "\n")
-
-        return jsonify({"message": bot_response})
+        return jsonify({
+            'message': bot_response,
+        })
     else:
-        return jsonify({"message": "Please ask a question related to cloud infrastructure."})
+        return jsonify({
+            'message': 'Please ask a cloud infrastructure-related question.',
+        })
+
+def is_cloud_infrastructure_question(question):
+    # Define conditions to identify cloud infrastructure questions
+    keywords = [
+        "cloud",
+        "infrastructure",
+        "deploy",
+        "scale",
+        "network",
+    ]
+
+    for keyword in keywords:
+        if keyword in question.lower():
+            return True
+
+    return False
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

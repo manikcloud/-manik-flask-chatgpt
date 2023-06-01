948169350b495d75cbc030ee5a5987acae47797e
Kumar <varkumar@deloitte.com>
2023-06-02 01:40:02 +0800

    dynamo-db

---
+++ infra/dynamodb.tf
@@ -0,0 +1,26 @@
+resource "aws_dynamodb_table" "user_table" {
+  name           = "manik-gpt-auth"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  range_key      = "email"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  attribute {
+    name = "password"
+    type = "S"
+  }
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+}
